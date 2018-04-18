c521dbe955b4ba741f433af275de6d15292b61ca
EdgarLGB <guobao.li@atos.net>
2018-04-18 15:09:10 +0200

    Modify the analyser

--- src/main/scala/demo/MockAnalyser.scala
+++ src/main/scala/demo/MockAnalyser.scala
@@ -1,13 +1,13 @@
 package demo
 
-import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Analyse the logs
   */
 object MockAnalyser {
-
 
   def main(args: Array[String]): Unit = {
     val inputDirPath = args(0)
@@ -26,9 +26,26 @@ object MockAnalyser {
 
     // execute sql query
     df.createOrReplaceTempView("logs")
-    val sqlQuery = "SELECT source, timestamp, COUNT(*) as occurrence, 'The source attempted to request too many times in the same second' as message FROM logs GROUP BY source, timestamp HAVING occurrence >= %d ORDER BY occurrence DESC".format(occurrence)
-    val queryDF = spark.sql(sqlQuery).coalesce(1)
-    queryDF.write.json(outputPath)
+    val sqlQuery = "SELECT source, timestamp, COUNT(*) as occurrence FROM logs GROUP BY source, timestamp HAVING occurrence >= %d ORDER BY occurrence DESC".format(occurrence)
+    val queryDF = spark.sql(sqlQuery)
 
+    queryDF.show(10)
+
+    // extract and construct the string
+    import spark.implicits._
+    val messages = queryDF.map(r => {
+      val source = r.getString(0)
+      val timestamp = r.getString(1)
+      val num = r.getLong(2)
+      "Source %s has %d requests in the moment %s".format(source, num, timestamp)
+    }).collect()
+
+
+    // recreate a dataframe with the generated message
+    // convert array to df
+    val rows = sc.parallelize(messages).map { x => Row(x) }.coalesce(1)
+    val messageDF = spark.createDataFrame(rows, StructType(Seq(StructField("message", StringType, false))))
+
+    messageDF.write.json(outputPath)
   }
 }
